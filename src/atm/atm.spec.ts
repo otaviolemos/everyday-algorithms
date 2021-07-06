fabe8cba2b46b5181fb79a8829ef1701c1a49683
Otavio Lemos <otaviolemos@gmail.com>
2021-07-06 13:41:13 -0300

    refactor: make equals shorter

--- src/atm/atm.spec.ts
+++ src/atm/atm.spec.ts
@@ -27,16 +27,10 @@ describe('ATM', () => {
   }
 
   function areEquals (arr1: number[], arr2: number[]): boolean {
-    if (arr1.length !== arr2.length) {
-      return false
-    }
-
+    if (arr1.length !== arr2.length) return false
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false
-      }
+      if (arr1[i] !== arr2[i]) return false
     }
-
     return true
   }
 })
